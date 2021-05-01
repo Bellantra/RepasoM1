df123be4b22edd0b1774fe8936c7f51331e0dcb7
Bellantra <yamilapaez2@hotmail.com>
2021-05-01 23:00:19 +0200

    resuelto

--- 03-Sum-Zero/sum-zero.js
+++ 03-Sum-Zero/sum-zero.js
@@ -7,5 +7,19 @@
  * @param {Array} arr1 - esto es un array.
  * @returns {Array || undefined} - deberia retornar un array o undefined.
  */
+  //arr=[1,4,8,7,-1]
+  //arr=[1,2,3,4,5,6,7]          USE ESTOS 3 DE PRUEBA
+  //arr=[1,2-13-2]
 
-function sumZero(arr) {}
+function sumZero(arr) {
+    var nuevoArray;
+    for(var i=0;i < arr.length;i++){
+      for(var j=1;j<arr.length;j++){       
+          if (arr[i]+arr[j]===0){
+              nuevoArray = [arr[i],arr[j]];
+              return nuevoArray;
+          }
+      }
+    }
+    return nuevoArray;
+}

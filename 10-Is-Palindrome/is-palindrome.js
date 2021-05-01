7fbd3b6a5b0cf2ce44bde55fd05fe4b7868f844d
Bellantra <yamilapaez2@hotmail.com>
2021-05-01 23:25:16 +0200

    resuelto

--- 10-Is-Palindrome/is-palindrome.js
+++ 10-Is-Palindrome/is-palindrome.js
@@ -7,4 +7,24 @@
  * @returns {String} - deberia retornar un string.
  */
 
-function isPalindrome(str) {}
+function isPalindrome(str) {
+    var arr=str.split('');
+    var reverse = arr.reverse();
+    
+    reverse= reverse.join('');
+
+    if(str===reverse) return true;
+
+    return false;
+}
+//isPalindrome("neuquen");
+//isPalindrome("laura");
+
+//Recursivo
+/* function isPalindrome(str) {
+    if (str.length === 0) return true;
+  
+    let last = str.length - 1;
+    if (str[0] === str[last]) return isPalindrome(str.substr(1, last - 1));
+    return false;
+  } */

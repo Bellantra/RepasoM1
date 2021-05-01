0f89ebe76b91aac3b9821764959a69533d23df34
Bellantra <yamilapaez2@hotmail.com>
2021-05-01 23:12:20 +0200

    resuelto

--- 08-Power/power.js
+++ 08-Power/power.js
@@ -8,4 +8,20 @@
  * @returns {Array || undefined} - deberia retornar un array o undefined.
  */
 
-function power(base, exp) {}
+function power(base, exp) {
+    
+   if((base||exp) < 0) return 0;             
+
+   var exponente = base ** exp;
+   
+   return exponente;
+}
+
+//power(2,2);
+
+//Con recursion
+
+/* function power(base, exp) {
+   if (exp === 0) return 1;
+   return base * power(base, exp - 1);
+ } */

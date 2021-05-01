a128a74b1e0f8b33ccbe99ddcd207eb96eb5baaf
Bellantra <yamilapaez2@hotmail.com>
2021-05-01 23:10:34 +0200

    resuelto

--- 07-Factorial/factorial.js
+++ 07-Factorial/factorial.js
@@ -8,4 +8,9 @@
  * @returns {Number} - deberia retornar un number.
  */
 
-function factorial(num) {}
+function factorial(num) {
+
+    if(num===1 || num===0) return 1;
+
+    return num * factorial(num-1);
+}

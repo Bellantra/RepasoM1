4e216e5f6115d245cc638505bdbf1f9fae5550ce
Bellantra <yamilapaez2@hotmail.com>
2021-05-01 23:04:49 +0200

    resuelto

--- 04-Average-Pair/average-pair.js
+++ 04-Average-Pair/average-pair.js
@@ -8,4 +8,17 @@
  * @returns {Array || undefined} - deberia retornar un array o undefined.
  */
 
-function averagePair(arr, num) {}
+//arr=[1,3,7,4,5,3]   num=6
+//arr=[1,3,7,4,5,3]   num=100 
+
+function averagePair(arr, num) {
+
+    for(var i=0;i < arr.length; i++){
+        for(var j=i+1;j<arr.length;j++){
+            if((arr[i]+arr[j])/2===num){
+                return true;
+            }
+        }
+    }
+    return false;
+}

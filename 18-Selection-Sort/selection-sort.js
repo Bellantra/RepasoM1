25acacf74264ca35f7912554b34d6363677f32e8
Bellantra <yamilapaez2@hotmail.com>
2021-05-04 17:09:55 +0200

    resuelto, pasado en consola

--- 18-Selection-Sort/selection-sort.js
+++ 18-Selection-Sort/selection-sort.js
@@ -5,18 +5,18 @@ function selectionSort(arr) {
   //Arranca siempre el recorrido de la linea siguiente, dejando adelante los menores ordenados.
   var pos;
   
-
+  //Estructure ppal que recorre todo el arreglo 
   for(var i = 0; i < arr.length-1; i++){
-    var menor=undefined;
-    for(var j = i ; j < arr.length -1; j++){
+    var menor=undefined;                        //arranca undefined en cada inicio de este for, para saber si hay algun numero menos¡r para cambiar
+    for(var j = i ; j < arr.length -1; j++){   //recorro siempre desde el i en adelante porque delante del i esta ordenado siempre
       
       if(arr[j+1] < arr[j]){
-         menor = arr[j+1];
-         pos = j+1;           
+         menor = arr[j+1];    //guardo menor valor
+         pos = j+1;           //guardo la posicion donde esta ese valor menor(la voy a necesitar para cambiar) 
       }   
       
     }
-    if(menor!==undefined){
+    if(menor!==undefined){    //si encontre uno menor al valor del i, realizo el cambio
       var aux = arr[i];
       arr[i] = menor;
       arr[pos] = aux;      

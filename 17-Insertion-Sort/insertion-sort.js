834db9f0806e124612f0845d9f158df9e5b1d791
Bellantra <yamilapaez2@hotmail.com>
2021-05-04 14:42:56 +0200

    resuelto y probado

--- 17-Insertion-Sort/insertion-sort.js
+++ 17-Insertion-Sort/insertion-sort.js
@@ -1,3 +1,28 @@
+const { defaultMaxListeners } = require("node:events")
+
 function insertionSort(arr) {
-  // construye un algoritmo de busqueda bubble sort
+  // construye un algoritmo de busqueda insertion sort
+
+ //recorro comparando cada pos con todas las anteriores del array; compara si es menor con todas las que estan 
+ //delante de ella. Van quedando siempre ordenados las primeras posiciones. Cuando acomoda la ultima posicion
+ //el array ya queda ordenado.
+  
+ var aux; 
+ //Esta estructura recorre el array de principio a fin
+ for(let i=0; i < arr.length; i++){
+  for(var j=i; j >= 0; j--){
+    if(arr[j+1] < arr[j]){
+      aux = arr[j];
+      arr[j] = arr[j+1];
+      arr[j+1]=aux;
+    }     
+  }
 }
+ //Retorno array Ordenado
+ return arr;  
+}
+
+/* var array=[5,9,7,1,4,3,25,40];
+var arrOrdenado=insertionSort(array);           //PRUEBA EN CONSOLA.
+console.log(arrOrdenado);
+ */
